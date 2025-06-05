56cbcbc97c18cefbc03a1672d25f7a2e7ace56db
Alberto Noguchi <alberto2611@gmail.com>
2025-06-05 15:32:40 -0300

    first commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+  private static void Main(string[] args)
+  {
+    Console.WriteLine("Hello, Master!");
+    int sum = AddNumbers(1, 1);
+    Console.WriteLine(sum + 10);
+    Console.WriteLine(AddNumbers(2));
+    Console.WriteLine(AddNumbers(1, 2, 3, 4, 5, 6, 7, 8));
+  }
+  public static int AddNumbers(int a, int b = 5)
+  {
+    return a + b;
+  }
+  public static int AddNumbers(int a, int b, int c)
+  {
+    return a + b + c;
+  }
+
+  public static int AddNumbers(params int[] integers)
+  {
+    return integers.Sum();
+  }
+}
