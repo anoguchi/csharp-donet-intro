7cf6c19a598de26f86bb2fcc4ec6017a6b98d102
Alberto Noguchi <alberto2611@gmail.com>
2025-06-06 17:10:48 -0300

    CSharp Built-in Types

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Security.Cryptography;
+
+internal class Program
 {
   private static void Main(string[] args)
   {
@@ -7,6 +9,53 @@
     Console.WriteLine(sum + 10);
     Console.WriteLine(AddNumbers(2));
     Console.WriteLine(AddNumbers(1, 2, 3, 4, 5, 6, 7, 8));
+
+    string name = "Alberto";
+    name = name.ToLower();
+
+    Console.WriteLine(name);
+    Console.WriteLine(DateTime.UtcNow);
+
+    DateTime.UtcNow.AddDays(1);
+    Console.WriteLine(DateTime.UtcNow.AddDays(1));
+
+    DateTime d = DateTime.Parse("10/9/2024 4:43:48 PM");
+    Console.WriteLine(d);
+
+    // Arrays
+    int[] arrayOfIntegers = [1, 2, 3, 4, 5];
+
+    foreach (char character in "Hello World")
+    {
+      Console.WriteLine(character);
+    }
+
+    var employeeType = EmployeeType.Manager;
+    int employeeInt = (int)EmployeeType.Worker;
+    Console.WriteLine(employeeType);
+    Console.WriteLine(employeeInt);
+
+    // Tupples
+    var myTuple = (42, "Hello", true);
+    var myNamedTuple = (Age: 30, Name: "Beto", IsEmployed: false);
+    Console.WriteLine(myTuple.Item1);
+    Console.WriteLine(myTuple.Item2);
+    Console.WriteLine(myTuple.Item3);
+    Console.WriteLine(myNamedTuple.Age);
+    Console.WriteLine(myNamedTuple.Name);
+    Console.WriteLine(myNamedTuple.IsEmployed);
+
+    var personInfo = GetEmployee();
+    Console.WriteLine(personInfo.Age);
+    Console.WriteLine(personInfo.Name);
+
+  }
+  // Enums
+  enum EmployeeType
+  {
+    Manager = 1,
+    Supervisor = 2,
+    Worker = 3
   }
   public static int AddNumbers(int a, int b = 5)
   {
@@ -31,5 +80,9 @@
       return true;
     }
     return false;
+  }
+  public static (int Age, String Name) GetEmployee()
+  {
+    return (30, "Beto");
   }
 }
--- MyFirstApp/obj/Debug/net9.0/MyFirstApp.AssemblyInfo.cs
+++ MyFirstApp/obj/Debug/net9.0/MyFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+819e3e2009c75f689e42f5a0606cf1efcef31efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
