fcf6130811511a336a1d0941ebc21306ba1d6a5e
Alberto Noguchi <alberto2611@gmail.com>
2025-06-13 17:19:37 -0300

    dispose pattern

--- CommonLibrariesAndAdvancedTopics/Program.cs
+++ CommonLibrariesAndAdvancedTopics/Program.cs
@@ -1,5 +1,4 @@
-﻿
-internal class Program
+﻿internal class Program
 {
     public static async Task Main(string[] args)
     {
@@ -12,5 +11,23 @@ internal class Program
         using var httpClient = new HttpClient();
         var response = await httpClient.GetStringAsync(url);
         return response;
+    }
+}
+
+public class MyResource : IDisposable
+{
+    private bool _disposed = false; // To detect redundant calls
+    private SqlConnection _connection;
+
+    // Implement IDisposable
+    public void Dispose()
+    {
+        if (!disposed)
+        {
+            _connection.Dispose();
+
+            // Free your own state (unmanaged objects).
+            disposed = true;
+        }
     }
 }
