a0f61f344e7cf9a0b79b6499043b2bf7f5dc8cb7
Alberto Noguchi <alberto2611@gmail.com>
2025-06-14 17:12:39 -0300

    refactor bug

--- TheEmployeeAPI/Employee.cs
+++
@@ -1,8 +0,0 @@
-namespace TheEmployeeAPI;
-
-public class Employee
-{
-    public int Id { get; set; }
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
-}
