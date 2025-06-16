ac230061f6ff2d4098a58c650a67f1a22c9d5e91
Alberto Noguchi <alberto2611@gmail.com>
2025-06-16 15:57:00 -0300

    refactor the api with controllers

--- EmployeeAPI/EmployeeAPI/Program.cs
+++ EmployeeAPI/EmployeeAPI/Program.cs
@@ -14,11 +14,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IRepository<Employee>,  EmployeeRepository>();
 builder.Services.AddProblemDetails();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-
+builder.Services.AddControllers();
 
 var app = builder.Build();
-
-var employeeRoute = app.MapGroup("employees");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,87 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-//  NEVER EVER returns anything that isn't tied directly to an HTTP status code.
-employeeRoute.MapGet(string.Empty, (IRepository<Employee> repository) => {
-    var employees = repository.GetAll();
-    return Results.Ok(employees.Select(employee => new GetEmployeeResponse {
-        FirstName = employee.FirstName,
-        LastName = employee.LastName,
-        Address1 = employee.Address1,
-        Address2 = employee.Address2,
-        City = employee.City,
-        State = employee.State,
-        ZipCode = employee.ZipCode,
-        PhoneNumber = employee.PhoneNumber,
-        Email = employee.Email
-    }));
-});
-
-// Spencer always recommends using binding attributes.
-employeeRoute.MapGet("{id:int}", ([FromRoute]int id, IRepository<Employee> repository) => {
-    var employee = repository.GetById(id);
-    if (employee == null)
-    {
-        return Results.NotFound();
-    }
-
-    return Results.Ok(new GetEmployeeResponse {
-        FirstName = employee.FirstName,
-        LastName = employee.LastName,
-        Address1 = employee.Address1,
-        Address2 = employee.Address2,
-        City = employee.City,
-        State = employee.State,
-        ZipCode = employee.ZipCode,
-        PhoneNumber = employee.PhoneNumber,
-        Email = employee.Email
-    });
-});
-
-employeeRoute.MapPost(string.Empty, 
-    async ([FromBody] CreateEmployeeRequest employeeRequest, 
-        IRepository<Employee> repository,
-        IValidator<CreateEmployeeRequest> validator) => {
-        var validationResults = await validator.ValidateAsync(employeeRequest);
-        if (!validationResults.IsValid)
-        {
-            return Results.ValidationProblem(validationResults.ToDictionary());
-        } 
-        var newEmployee = new Employee {
-            FirstName = employeeRequest.FirstName!,
-            LastName = employeeRequest.LastName!,
-            SocialSecurityNumber = employeeRequest.SocialSecurityNumber,
-            Address1 = employeeRequest.Address1,
-            Address2 = employeeRequest.Address2,
-            City = employeeRequest.City,
-            State = employeeRequest.State,
-            ZipCode = employeeRequest.ZipCode,
-            PhoneNumber = employeeRequest.PhoneNumber,
-            Email = employeeRequest.Email
-        };
-    repository.Create(newEmployee);
-    return Results.Created($"/employees/{newEmployee.Id}", employeeRequest);
-});
-
-employeeRoute.MapPut("{id:int}", ([FromBody] UpdateEmployeeRequest employeeRequest, [FromRoute]int id, [FromServices] IRepository<Employee> repository) => {
-    var existingEmployee = repository.GetById(id);
-    if (existingEmployee == null)
-    {
-        return Results.NotFound();
-    }
-
-    existingEmployee.Address1 = employeeRequest.Address1;
-    existingEmployee.Address2 = employeeRequest.Address2;
-    existingEmployee.City = employeeRequest.City;
-    existingEmployee.State =    employeeRequest.State;
-    existingEmployee.ZipCode = employeeRequest.ZipCode;
-    existingEmployee.PhoneNumber = employeeRequest.PhoneNumber;
-    existingEmployee.Email = employeeRequest.Email;
-    
-    repository.Update(existingEmployee);
-    return Results.Ok(existingEmployee);
-});
+app.MapControllers();
 
 app.Run();
 
